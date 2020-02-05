cc1887d0111e277bf955b4f38fbd5d3cb738210c
Clement Lefeu <clefeu@fsl.radishng.local>
2020-02-04 19:40:31 -0600

    assign02 on matlab

--- Assign02.m
+++ Assign02.m
@@ -1,16 +1,15 @@
 clear all;
 clc;
 
-filename = 'Declaration_of_Independance.txt';
+filename = 'text.txt';
 fileID = fopen(filename,'r'); %Open File
 if (fileID > 0) % check if the pipe opened correctly
     data = fscanf(fileID,'%c'); % Write file into a variable
     fclose(fileID); % Close the pipe
 end
 bin = dec2bin(data,8); % translate text to binary
-filename = 'Declaration_of_Independance.txt';
 fileID = fopen('binary_text.txt','w'); %Open File
 if (fileID > 0) % check if the pipe opened correctly
-    data = fprintf(fileID,'%s', bin); % Write file into a variable
+    fprintf(fileID,'%c', bin'); % Write file into a variable
     fclose(fileID); % Close the pipe
 end
